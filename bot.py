7af1e5bae926f7099c5a10faa8799da1aa596e90
Abdelaziz Abu Hamza <abdelaziz.mirasoft@gmail.com>
2021-04-29 16:28:59 +0100

    doTwitterTweet: fully implemented and runs successfully

--- bot.py
+++ bot.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+# Display exception
+import traceback
 import time
 import re
 import math
@@ -22,8 +27,14 @@ def loadDriver():
     # To ignore the check of the SSL Certificate
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
+    options.add_argument("start-maximized")
+    # start chrome without showing the browser
+    # options.add_argument('headless')
+    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # END start chrome without showing the browser
     driver = webdriver.Chrome(executable_path=ChromeDriverManager(
     ).install(), options=options)
+
     driver.get('https://followfast.com/login.php')
     return driver
 
@@ -119,14 +130,15 @@ def doFbSubs():
         except:
             print("No Follow button found")
             foundButton = False
-    time.sleep(randint(10, 20))
+    time.sleep(randint(5, 10))
 
 
 def doInstaLikes():
     global driver
     driver.find_element_by_css_selector("a[href*='instb.php'] ").click()
     foundButton = True
-    while foundButton:
+    index = 0
+    while foundButton and index < 20:
         try:
             element = driver.find_element_by_css_selector(
                 "div.likebox0")
@@ -136,6 +148,7 @@ def doInstaLikes():
                 time.sleep(randint(5, 10))
                 closeTabs(1)
                 time.sleep(randint(5, 15))
+                index += 1
             else:
                 foundButton = False
         except:
@@ -144,50 +157,60 @@ def doInstaLikes():
     time.sleep(randint(10, 20))
 
 
-def doTwitterTweet():
+def twitterWorkFlow(TweetOrLoveOrRetwt):
+    global driver
+    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+    driver.find_element_by_xpath("//input[@value='Tweet']").click()
+    time.sleep(1)
+    moveToTab(0)
+    time.sleep(2)
+    driver.find_element_by_xpath("//input[@value='Tweet']").click()
+    # print("We switch to the last opened tab")
+    moveToTab(-1)
+    # print("set_page_load_timeout")
+    driver.set_page_load_timeout(0.001)
+    time.sleep(0.1)
+    # print("Kill last click")
+    try:
+        webdriver.ActionChains(driver).send_keys(
+            Keys.ESCAPE).perform()
+    except:
+        """Do nothing"""
+    time.sleep(1)
+    # print("We switch to the tweet page")
+    moveToTab(1)
+    # Do Tweets
+    time.sleep(5)
+    doTwitterTweet()
+    time.sleep(3)
+    # print("We switch to main page")
+    moveToTab(0)
+    time.sleep(1)
+    # print("We close the tweet page")
+    closeTabs(1)
+    time.sleep(1)
+    # print("We switch to main page")
+    moveToTab(0)
+    time.sleep(1)
+    closeTabs(1)
+
+
+def doTwitter():
     global driver
     driver.find_element_by_css_selector("a[href*='tweet.php'] ").click()
     time.sleep(5)
     foundButton = True
-    while foundButton:
+    index = 0
+    while foundButton and index < 20:
         try:
             element = driver.find_element_by_css_selector(
                 "div.likebox0")
             attributeValue = element.get_attribute("style")
             # This value may change from time to time so you should update
             if "50.jpg" in attributeValue:
-                webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-                driver.find_element_by_xpath("//input[@value='Tweet']").click()
-                time.sleep(1)
-                moveToTab(0)
-                time.sleep(2)
-                driver.find_element_by_xpath("//input[@value='Tweet']").click()
-                # print("We switch to the last opened tab")
-                moveToTab(-1)
-                # print("set_page_load_timeout")
-                driver.set_page_load_timeout(0.001)
-                time.sleep(0.1)
-                # print("Kill last click")
-                try:
-                    webdriver.ActionChains(driver).send_keys(
-                        Keys.ESCAPE).perform()
-                except:
-                    """Do nothing"""
-                time.sleep(1)
-                # print("We switch to the tweet page")
-                moveToTab(1)
-                time.sleep(5)
-                # print("We switch to main page")
-                moveToTab(0)
-                time.sleep(1)
-                # print("We close the tweet page")
-                closeTabs(1)
-                time.sleep(1)
-                # print("We switch to main page")
-                moveToTab(0)
-                time.sleep(1)
-                closeTabs(1)
-                break
+                twitterWorkFlow("Tweet")
+                index += 1
+                time.sleep(randint(5, 10))
             else:
                 foundButton = False
         except:
@@ -195,6 +218,27 @@ def doTwitterTweet():
             foundButton = False
     print("We sleep randint(10, 20)")
     time.sleep(randint(10, 20))
+
+
+def doTwitterTweet():
+    global driver
+    driver.set_page_load_timeout(30)
+    print("Set random to avoid twitter existing post warning ")
+    try:
+        span = driver.find_element_by_xpath(
+            "/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[1]/div/div/div/div/div[2]/div[1]/div/div/div/div/div/div[1]/div/div/div/div[1]/div/div/div/div/div/div/div/div/span[2]/span")
+        driver.execute_script(
+            "arguments[0].innerText = '"+str(randint(100, 900))+"'", span)
+    except:
+        """ DO NOTHING """
+    time.sleep(0.5)
+    print("Click Tweet button")
+    try:
+        driver.find_element_by_xpath(
+            "//div[@role='button'][@data-testid='tweetButton'][@tabindex='0']").click()
+        webdriver.ActionChains(driver).send_keys(Keys.ENTER).perform()
+    except Exception as e:
+        print(e)
 
 
 def doTwitterLikes():
@@ -210,45 +254,26 @@ def doTwitterRetweet():
 
 
 def doTasks():
-    # doFbSubs()
-    # doInstaLikes()
-    doTwitterTweet()
-    # doTwitterLikes()
-    # doTwitterRetweet()
-    time.sleep(randint(720, 1020))
+    for i in range(3):
+        doFbSubs()
+        doInstaLikes()
+        doTwitter()
+        # doTwitterLikes()
+        # doTwitterRetweet()
+    sleepingTime = randint(720, 1020)
+    print("sleepingTime: "+str(sleepingTime/60))
+    time.sleep(sleepingTime)
 
 
 if __name__ == "__main__":
     try:
         driver = loadDriver()
-        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
         login(driver)
         while True:
-            doTasks()
-            # Sleep for a while to avoid bot behaviour detection
-            if dt.time() > datetime.time(23):
-                print("Time to sleep: 16.2 hours")
-                time.sleep(60*60*9.2)
-                print("We continue")
-            elif dt.time() >= datetime.time(12) and dt.time() < datetime.time(13):
-                print("Time to sleep: 1.3 hours")
-                time.sleep(60*60*1.3)
-                print("We continue")
-
+            try:
+                doTasks()
+            except:
+                """ Just ignore the exception to not lose the session"""
     except:
         print("Re-try")
         driver.quit()
-
-    # driver = loadDriver()
-    # login(driver)
-    # while True:
-    #     doTasks()
-    #     # Sleep for a while to avoid bot behaviour detection
-    #     if dt.time() > datetime.time(23):
-    #         print("Time to sleep: 16.2 hours")
-    #         time.sleep(60*60*9.2)
-    #         print("We continue")
-    #     elif dt.time() >= datetime.time(12) and dt.time() < datetime.time(13):
-    #         print("Time to sleep: 1.3 hours")
-    #         time.sleep(60*60*1.3)
-    #         print("We continue")
